72551295d3e9dd82274e6eebdf0b589ae259bfce
Sathwik <esathwikkumar@gmail.com>
2024-12-13 00:29:01 +0530

    made changes in the home page to make it adaptable for smaller screen sizes

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     <div className='w-full h-screen bg-[#A9C89A] p-20 flex relative'>
       <div className="w-1/2 h-full flex flex-col justify-between font-grotesk">
         <div className='heading'>
-          <h1 className='text-[8vw] uppercase leading-none -mb-10'>Take the First</h1>
-          <h1 className='text-[8vw] uppercase leading-none -mb-10'>Step to a</h1>
+          <h1 className='text-[8vw] uppercase leading-none -mb-7'>Take the First</h1>
+          <h1 className='text-[8vw] uppercase leading-none -mb-7'>Step to a</h1>
           <h1 className='text-[8vw] uppercase leading-none -mb-10'>Healthier Mind</h1>
         </div>
       </div>
